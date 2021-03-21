808bdc8ef6888ab5e5313ab07c92770799145480
Christian Lipp <christian.lipp@corba.at>
2021-03-21 21:18:43 +0100

    Migration to diskstation
    - Switched to Docker
    - Directory movement
    - Added device tracker
    - Extracted iPhone config for Christian
    - Integrated esphome via iframe (works only local)
    - Extracted esphome network settings

--- custom_components/update_hacs.sh
+++
@@ -1,4 +0,0 @@
-wget -O hacs.zip  `curl -s -L https://github.com/hacs/integration/releases/latest | grep -Eoi 'href="[^\"]+hacs.zip"' | sed 's*href="*https://www.github.com*' | sed 's/"//'`
-unzip -o hacs.zip -d hacs
-rm hacs.zip
-* ha host reboot
